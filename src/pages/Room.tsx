598ef9d2db3e09617153039979c9a5108ff56193
tavares97 <afpt.digital2013@gmail.com>
2021-06-24 12:34:39 +0100

    Added Room page and styling

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,7 +1,35 @@
+import logoImg from '../assets/images/logo.svg';
+import Button from '../components/Button';
+
+import '../styles/room.scss';
+
 const Room = () => {
 	return (
-		<div>
-			<h1>Sala</h1>
+		<div id='page-room'>
+			<header>
+				<div className='content'>
+					<img src={logoImg} alt='logo' />
+					<div>codigo sala</div>
+				</div>
+			</header>
+
+			<main>
+				<div className='room-title'>
+					<h1>sala react</h1>
+					<span>4 perguntas</span>
+				</div>
+
+				<form>
+					<textarea placeholder='Faça a sua questão?' />
+					<div className='form-footer'>
+						<span>
+							Para enviar uma pergunta,
+							<button>tem que estar autenticado</button>.
+						</span>
+						<Button type='submit'>Enviar Pergunta</Button>
+					</div>
+				</form>
+			</main>
 		</div>
 	);
 };
